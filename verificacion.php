aa9d0679e45380cfa32f3504ab47379efb940b2a
CAMILO <cardenascamilo1012@gmail.com>
2024-10-28 00:18:48 -0500

    cargue nuevo

--- verificacion.php
+++ verificacion.php
@@ -1,57 +1,48 @@
 <?php
-
 $servername = "localhost";
 $username_db = "root";
 $password_db = "";
 $dbname = "clinicadb";
 
 $conn = new mysqli($servername, $username_db, $password_db, $dbname);
+// Aquí puedes manejar el proceso de login
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$username = $_POST['loginDocument'];
+	$password = $_POST['loginPassword'];
 
-// Verifica si la conexión es correcta
+	// Verifica si la conexión es correcta
 if (!$conn) {
     die("Conexión fallida: " . mysqli_connect_error());
 }
 
-// Aquí puedes manejar el proceso de login
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['loginDocument'];
-    $password = $_POST['loginPassword'];
+// Preparar la consulta
+$stmt = $conn->prepare("SELECT * FROM medico WHERE documento = ? AND contrasena = ?");
 
-    // Preparar la consulta
-    $stmt = $conn->prepare("SELECT * FROM medico WHERE documento = ? AND contrasena = ?");
+if ($stmt === false) {
+    die("Error en la consulta SQL: " . $conn->error); // Muestra el error si la consulta falla
+}
 
-    if ($stmt === false) {
-        die("Error en la consulta SQL: " . $conn->error); // Muestra el error si la consulta falla
-    }
+// Asociar los parámetros (s = string)
+$stmt->bind_param("ss", $username, $password);
 
-    // Asociar los parámetros (s = string)
-    $stmt->bind_param("ss", $username, $password);
+// Ejecutar la consulta
+$stmt->execute();
 
-    // Ejecutar la consulta
-    $stmt->execute();
-
-    // Obtener los resultados
-    $registros = $stmt->get_result();
+// Obtener los resultados
+$registros = $stmt->get_result();
 
 if ($registros->num_rows > 0) {
     // Usuario encontrado, hacer algo
     echo "Inicio de sesión exitoso.";
-    header("Location: Perfil.html");
+    header("Location: historialclinico.php");
 } else {
     // Usuario no encontrado o credenciales incorrectas
     echo "Nombre de usuario o contraseña incorrectos."; // boton para devolver
-<<<<<<< HEAD
-    sleep(3); // Pausa la ejecución durante 3 segundos
-    header("Location: iniciosesion.html"); // redireciona al inicio de sesion
-=======
     echo '<a href="iniciosesion.html" class="btn btn-primary btn-lg mt-3">Volver a intentar</a>'; // Botón estilizado
->>>>>>> 9ca643acea00adc78bb82e00627c268a9da9c23b
 }
-
 // Cerrar la declaración
 $stmt->close();
 
-// Cerrar la conexión
-$conn->close();
 };
+
 ?>
